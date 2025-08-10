6d81140e9cb7af6dd758814a01a2741da7f0e7fd
josephus <otejerejosephus@gmail.com>
2025-08-10 23:04:08 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Welcome from './components/pages/Welcome.vue';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-gradient">Hello world</h1>
+  </div>
+  <Welcome>
+
+  </Welcome>
+</template>
+
+<style scoped>
+
+</style>
